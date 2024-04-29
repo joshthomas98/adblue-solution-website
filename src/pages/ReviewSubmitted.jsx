b7a57cc141197038d514e80d91db639815030441
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2024-04-30 00:23:26 +0100

    30/4/24 - 1

--- src/pages/ReviewSubmitted.jsx
+++ src/pages/ReviewSubmitted.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ReviewSubmitted = () => {
   return (
-    <div>
-      <h1>Review Submitted Successfully</h1>
-    </div>
+    <Container className="text-center">
+      <h2 className="pt-4 pb-3">Review Submitted Successfully!</h2>
+      <p>Thank you for your review. Your feedback is valuable to us.</p>
+      <Button as={Link} to="/" variant="primary" className="mt-3">
+        Go Back to Homepage
+      </Button>
+    </Container>
   );
 };
 
