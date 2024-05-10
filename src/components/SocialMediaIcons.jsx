850ca92bcda7b4a3db9f61e87784f9e0226ec69b
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2024-05-10 03:01:47 +0100

    FINAL

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -4,7 +4,7 @@ import {
   faFacebook,
   faTwitter,
   faInstagram,
-  faPinterest,
+  faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 
 const SocialMediaIcons = () => {
@@ -14,11 +14,7 @@ const SocialMediaIcons = () => {
     <div className="footer_profile">
       <ul className="social-icons">
         <li>
-          <a
-            href="https://www.facebook.com/profile.php?id=100076412066378"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="#" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebook} size={iconSize} />
           </a>
         </li>
@@ -34,7 +30,7 @@ const SocialMediaIcons = () => {
         </li>
         <li>
           <a href="#">
-            <FontAwesomeIcon icon={faPinterest} size={iconSize} />
+            <FontAwesomeIcon icon={faYoutube} size={iconSize} />
           </a>
         </li>
       </ul>
